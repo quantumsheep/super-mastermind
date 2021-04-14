53b8151fc18b97180e6d16fa35d09048be109e77
quantumsheep <nathanael.dmc@outlook.fr>
2021-04-15 00:39:43 +0200

    Add CLI "len" argument

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod mastermind;
 
+use clap::{App, Arg};
 use mastermind::*;
 use std::io::{self, Write};
 
-fn main() -> Result<(), io::Error> {
-    let mut game = MasterMind::new(4);
+fn mastermind_as_human(len: usize) -> Result<(), io::Error> {
+    let mut game = MasterMind::new(len);
     game.print_welcome();
 
     loop {
@@ -34,6 +35,25 @@ fn main() -> Result<(), io::Error> {
 
     println!();
     println!("Congratulation! Your beat the MasterMind!");
+
+    Ok(())
+}
+
+const DEFAULT_COLORS_COUNT: i32 = 4;
+
+fn main() -> Result<(), io::Error> {
+    let matches = App::new("MasterMind")
+        .args(&[Arg::new("len")
+            .long("len")
+            .short('l')
+            .about("Sets the length of the hidden composition")
+            .default_value(&DEFAULT_COLORS_COUNT.to_string())
+            .takes_value(true)])
+        .get_matches();
+
+    let len = matches.value_of_t("len").unwrap_or(4);
+
+    mastermind_as_human(len)?;
 
     Ok(())
 }
